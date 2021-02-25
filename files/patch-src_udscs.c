d5ed2b6a189dde52ef499c29406a3ac38412b3a6
Jonathan Fouquart <jfouquart@hotmail.fr>
2021-02-25 15:44:53 +0100

    rewrote patch to support multisession on fbsd13

--- files/patch-src_udscs.c
+++ files/patch-src_udscs.c
@@ -8,7 +8,7 @@
  #include <sys/un.h>
  #include <glib.h>
  #include <glib-unix.h>
-@@ -523,8 +524,13 @@ static void udscs_server_accept(struct udscs_server *s
+@@ -523,8 +524,17 @@ static void udscs_server_accept(struct udscs_server *s
      new_conn->read_callback = server->read_callback;
      new_conn->disconnect_callback = server->disconnect_callback;
  
@@ -17,7 +17,11 @@
 +    struct xucred peer_cred;
 +    length = sizeof(peer_cred);
 +    r = getsockopt(fd, SOL_LOCAL, LOCAL_PEERCRED, &peer_cred, &length);
++#if __FreeBSD__ >= 13
 +    new_conn->peer_cred.pid = peer_cred.cr_pid;
++#else
++    new_conn->peer_cred.pid = 0;
++#endif
 +    new_conn->peer_cred.uid = peer_cred.cr_uid;
 +    new_conn->peer_cred.gid = peer_cred.cr_groups[0];
 +
