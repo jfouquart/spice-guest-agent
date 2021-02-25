774192e555b901d09007708da50934700b8e5e7d
Jonathan Fouquart <jfouquart@hotmail.fr>
2021-02-25 15:14:26 +0100

    Add multisession support (only for fsb13)

--- files/patch-src_udscs.c
+++ files/patch-src_udscs.c
@@ -1,16 +1,25 @@
 --- src/udscs.c.orig	2018-06-06 09:27:15 UTC
 +++ src/udscs.c
-@@ -523,8 +523,14 @@ static void udscs_server_accept(struct udscs_server *s
+@@ -30,6 +30,7 @@
+ #include <unistd.h>
+ #include <errno.h>
+ #include <sys/socket.h>
++#include <sys/ucred.h>
+ #include <sys/un.h>
+ #include <glib.h>
+ #include <glib-unix.h>
+@@ -523,8 +524,13 @@ static void udscs_server_accept(struct udscs_server *s
      new_conn->read_callback = server->read_callback;
      new_conn->disconnect_callback = server->disconnect_callback;
  
-+#if 0
-     length = sizeof(new_conn->peer_cred);
-     r = getsockopt(fd, SOL_SOCKET, SO_PEERCRED, &new_conn->peer_cred, &length);
-+#else
-+    r = getpeereid(fd, &new_conn->peer_cred.uid, &new_conn->peer_cred.gid);
-+    new_conn->peer_cred.pid = 0; /* strtol(getenv("AGENT_PID"), NULL, 10); */
-+#endif
+-    length = sizeof(new_conn->peer_cred);
+-    r = getsockopt(fd, SOL_SOCKET, SO_PEERCRED, &new_conn->peer_cred, &length);
++    struct xucred peer_cred;
++    length = sizeof(peer_cred);
++    r = getsockopt(fd, SOL_LOCAL, LOCAL_PEERCRED, &peer_cred, &length);
++    new_conn->peer_cred.pid = peer_cred.cr_pid;
++    new_conn->peer_cred.uid = peer_cred.cr_uid;
++    new_conn->peer_cred.gid = peer_cred.cr_groups[0];
 +
      if (r != 0) {
          syslog(LOG_ERR, "Could not get peercred, disconnecting new client");
--- files/patch-src_udscs.h
+++ files/patch-src_udscs.h
@@ -1,16 +1,14 @@
 --- src/udscs.h.orig	2018-06-06 09:27:15 UTC
 +++ src/udscs.h
-@@ -171,6 +171,14 @@ int udscs_server_fill_fds(struct udscs_server *server,
+@@ -171,6 +171,12 @@ int udscs_server_fill_fds(struct udscs_server *server,
  void udscs_server_handle_fds(struct udscs_server *server, fd_set *readfds,
      fd_set *writefds);
  
-+#if 1
 +struct ucred {
 +  pid_t pid; /* Process ID of the sending process */
 +  uid_t uid; /* User ID of the sending process */
 +  gid_t gid; /* Group ID of the sending process */
 +};
-+#endif
 +
  /* Returns the peer's user credentials. */
  struct ucred udscs_get_peer_cred(struct udscs_connection *conn);
